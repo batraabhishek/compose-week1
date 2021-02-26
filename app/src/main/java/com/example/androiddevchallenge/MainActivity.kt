baa6f9367d411a5a8769019a5bc4065268ae62d8
Abhishek Batra <abhishekbatra1993@gmail.com>
2021-02-27 02:50:45 +0530

    klint

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,30 +20,33 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.navigation.compose.navigate
-
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
-import androidx.navigation.compose.rememberNavController
-import com.example.androiddevchallenge.data.Result
+import androidx.navigation.compose.*
 import com.example.androiddevchallenge.data.pupies.impl.BlockingFakePuppiesRepository
 import com.example.androiddevchallenge.data.pupies.impl.puppies
 import com.example.androiddevchallenge.data.successOr
@@ -51,7 +54,6 @@ import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.utils.loadPicture
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    val coroutineScope = rememberCoroutineScope()
+    rememberCoroutineScope()
     val navController: NavHostController = rememberNavController()
     NavHost(navController = navController, startDestination = "home") {
         composable("home") { Home(navController = navController) }
@@ -78,7 +80,6 @@ fun MyApp() {
             arguments = listOf(navArgument("puppyId") { type = NavType.StringType })
         ) { backStackEntry ->
             Details(
-                navController,
                 backStackEntry.arguments?.getString("puppyId")
             )
         }
@@ -162,7 +163,7 @@ fun PuppyRow(navController: NavHostController, puppy: Puppy) {
 }
 
 @Composable
-fun Details(navController: NavHostController, string: String?) {
+fun Details(string: String?) {
     val context = LocalContext.current
     val puppy: Puppy = BlockingFakePuppiesRepository(context)
         .getPuppy(string ?: "0")
@@ -193,10 +194,9 @@ fun Home(navController: NavHostController) {
         )
     }, content = {
         Surface(color = MaterialTheme.colors.background) {
-            val pups: List<Puppy> =
-                BlockingFakePuppiesRepository(context = context).getPuppies().successOr(
-                    emptyList()
-                )
+            BlockingFakePuppiesRepository(context = context).getPuppies().successOr(
+                emptyList()
+            )
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
